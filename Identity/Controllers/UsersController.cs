c66b8d80c757f15b141fd18e1ba72a51df45537c
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-09 23:46:45 +0300

    identity silme ve güncelleme işlemleri yapıldı

--- Identity/Controllers/UsersController.cs
+++ Identity/Controllers/UsersController.cs
@@ -44,5 +44,85 @@ namespace Identity.Controllers
             }
             return View(model);
         }
+
+        public async Task< IActionResult> Edit(string id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if(user != null)
+            {
+                return View(new EditViewModel
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email
+                });
+
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, EditViewModel model)
+        {
+            if(id != model.Id)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.Id);
+
+                if(user != null)
+                {
+                    user.Email = model.Email;
+                    user.FullName = model.FullName;
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if(result.Succeeded && !string.IsNullOrEmpty(model.Password))
+                    {
+                        await _userManager.RemovePasswordAsync(user);
+                        await _userManager.AddPasswordAsync(user, model.Password);
+                    }
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+                    foreach (IdentityError err in result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+                }
+            }
+
+            return View(model); 
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if(user != null)
+            {
+                await _userManager.DeleteAsync(user);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
