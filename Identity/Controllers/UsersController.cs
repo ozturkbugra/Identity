7620f300bd56fd5c7a0f18b4b3ce55622e205711
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-09 13:09:12 +0300

    kullanıcı ekleme yapıldı

--- Identity/Controllers/UsersController.cs
+++ Identity/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
@@ -15,6 +16,32 @@ namespace Identity.Controllers
         public IActionResult Index()
         {
             return View(_userManager.Users.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.UserName, Email = model.Email };
+
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password!);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                
+            }
+            return View(model);
         }
     }
 }
---
+++ Identity/ViewModels/CreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.ViewModels
+{
+    public class CreateViewModel
+    {
+        [Required]
+        public string? UserName { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Parolalar uyuşmuyor")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
