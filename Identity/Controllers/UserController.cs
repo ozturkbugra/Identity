ce887120807d77a3ea5623fe51a9293270d7af1c
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-09 11:02:57 +0300

    SeedData ile program açıldığında admin kullanıcısı ekleme

---
+++ Identity/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Identity/Models/IdentitySeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Admin_123";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(adminUser);
+
+            if(user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = adminUser,
+                    Email = "admin@bugraozturk.com",
+                    PhoneNumber = "5555555",
+
+                };
+
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+
+    }
+}
