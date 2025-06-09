415b38d6956e188a232a07ca332de34b801fd5ba
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-09 10:51:35 +0300

    Identity veri tabanı oluşturuldu

--- Identity/Models/IdentityContext.cs
+++ Identity/Models/IdentityContext.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 
 namespace Identity.Models
 {
     public class IdentityContext : IdentityDbContext<IdentityUser>
     {
-        public IdentityContext(DbContextOptions<IdentityContext> options):base(options)
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
         {
-               
         }
+
+       
     }
 }
