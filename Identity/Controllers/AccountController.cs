a677317b55b9f3fa52781c128db4bca48d4cf007
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-10 23:32:43 +0300

    Giriş yapma kodları eklendi

---
+++ Identity/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using Identity.Models;
+using Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+        private RoleManager<AppRole> _roleManager;
+        private SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if(user != null)
+                {
+                    await _signInManager.SignOutAsync();// ilk önce var olan cookie sıfırlansın
+
+                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, true);
+
+                    if (result.Succeeded)
+                    {
+                        //başarılıysa yapılan eski girişleri sıfırlayalım
+                        await _userManager.ResetAccessFailedCountAsync(user);
+                        await _userManager.SetLockoutEndDateAsync(user,null);
+
+                        return RedirectToAction("Index","Home");
+                    }else if (result.IsLockedOut)
+                    {
+                        var lockOutDate = await _userManager.GetLockoutEndDateAsync(user);
+                        var timeLeft = lockOutDate.Value - DateTime.UtcNow;
+                        ModelState.AddModelError("", $"Hesabınız kilitlendi, Lütfen {timeLeft.Minutes} dakika sonra deneyiniz!");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Parolanız yanlıştır.");
+
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Bu Email kayıtlı değildir.");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Identity/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.ViewModels
+{
+    public class LoginViewModel
+    {
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; } = null!;
+
+        public bool RememberMe { get; set; } = true;
+
+    }
+}
